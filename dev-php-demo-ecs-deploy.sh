79ec65cf1250a256db008249b035b424208d24ea
Shayakh Ahmed Rezoan <r4redu@gmail.com>
2020-05-29 11:03:02 +0600

    Update dev-php-demo-ecs-deploy.sh

--- dev-php-demo-ecs-deploy.sh
+++ dev-php-demo-ecs-deploy.sh
@@ -3,6 +3,9 @@ echo "$ECR_IMAGE"
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition dev-$1 --region us-east-1)
 echo "$TASK_DEFINITION"
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
+echo "$TASK_DEFINITION"
 NEW_TASK_INFO=$(aws ecs register-task-definition --region us-east-1 --cli-input-json "$NEW_TASK_DEFINTIION")
+echo "$NEW_TASK_INFO"
 NEW_REVISION=$(echo $NEW_TASK_INFO | jq '.taskDefinition.revision')
-aws ecs update-service --cluster ecs-cluster-php-demo --service service-dev-$1 --task-definition dev-$1:${NEW_REVISION} --region us-east-1
+echo "$NEW_REVISION"
+aws ecs update-service --cluster php-demo-ecs-cluster --service service-dev-$1 --task-definition dev-$1:${NEW_REVISION} --region us-east-1
