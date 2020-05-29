bb14dd228bd9da7bbcc89595c24b9476bd6500f4
Shayakh Ahmed Rezoan <r4redu@gmail.com>
2020-05-29 16:23:19 +0600

    Update dev-php-demo-ecs-deploy.sh

--- dev-php-demo-ecs-deploy.sh
+++ dev-php-demo-ecs-deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 ECR_IMAGE="700707367057.dkr.ecr.us-east-1.amazonaws.com/$1:$2"
-//echo "image Name: $ECR_IMAGE"
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition ecs-$1 --region us-east-1)
-//echo "Task Def Found: $TASK_DEFINITION"
-NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
-//echo "$IMAGE"
+NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $ECR_IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
 echo "$NEW_TASK_DEFINTIION"
 NEW_TASK_DEFINTIION=`echo $NEW_TASK_DEFINTIION | sed 's/\\r//g'`
 echo "$NEW_TASK_DEFINTIION"
